2a7616bfc2ecccd39d230dd743ba81ffc6e538ad
Thiago Alves <thiago_marketingdigital@hotmail.com>
2021-06-07 04:32:31 -0300

    test: :white_check_mark: add tests

--- sum_test.go
+++ sum_test.go
@@ -9,3 +9,27 @@ func TestSum(t *testing.T) {
 		t.Error("The result must be 5")
 	}
 }
+
+func TestSub(t *testing.T) {
+	result := sub(5, 3)
+
+	if result != 2 {
+		t.Error("The result must be 2")
+	}
+}
+
+func TestTimes(t *testing.T) {
+	result := times(2, 3)
+
+	if result != 6 {
+		t.Error("The result must be 6")
+	}
+}
+
+func TestSumX(t *testing.T) {
+	result := sumX(2, 3)
+
+	if result != 7 {
+		t.Error("The result must be 7")
+	}
+}
